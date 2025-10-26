88a91ebf38448113e972fe18e1f9341917d53089
Vibhu s <vibhudendra.s@gmail.com>
2025-10-26 23:06:28 +0530

    About Section

--- src/app.jsx
+++ src/app.jsx
@@ -3,15 +3,17 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
 import Menu from './Components/Menu';
+import About from './Components/About';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
     return (
         <main>
-            <Navbar />
+            <Navbar/>
             <Hero/>
             <Menu/>
+            <About/>
         </main>
     )
 }
